d9c334267857ce6ddebf32e43b3215ec2b12d3ae
Calle Dybedahl <calle@init.se>
2009-05-29 09:22:20 +0200

    Add the basic module structure

---
+++ Zonestat/lib/Zonestat/Prepare.pm
@@ -0,0 +1,41 @@
+package Zonestat::Prepare;
+
+use 5.008008;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+1;
+__END__
+
+=head1 NAME
+
+Zonestat::Prepare - preparatory tasks for statistics gathering
+
+=head1 SYNOPSIS
+
+  use Zonestat::Prepare;
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<Zonestat>.
+
+=head1 AUTHOR
+
+Calle Dybedahl, E<lt>calle@init.seE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2009 by Calle Dybedahl
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.9 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
---
+++ Zonestat/lib/Zonestat/Present.pm
@@ -0,0 +1,41 @@
+package Zonestat::Present;
+
+use 5.008008;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+1;
+__END__
+
+=head1 NAME
+
+Zonestat::Present - present gathered statistics
+
+=head1 SYNOPSIS
+
+  use Zonestat::Present;
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<Zonestat>.
+
+=head1 AUTHOR
+
+Calle Dybedahl, E<lt>calle@init.seE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2009 by Calle Dybedahl
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.9 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
