f1efa46263d95a3e01a52623f7aaa7cd993ec731
Calle Dybedahl <calle@init.se>
2009-05-29 09:11:42 +0200

    Basic framework from h2xs with minor edits.

---
+++ Zonestat/lib/Zonestat.pm
@@ -0,0 +1,41 @@
+package Zonestat;
+
+use 5.008008;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+1;
+__END__
+
+=head1 NAME
+
+Zonestat - gather and present statistics for a DNS zone
+
+=head1 SYNOPSIS
+
+  use Zonestat;
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<DNSCheck>.
+
+=head1 AUTHOR
+
+Calle Dybedahl, E<lt>calle@init.seE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2009 by Calle Dybedahl
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.9 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
