6307ac5424e94cb8e5ab7338880a9648c1018047
Calle Dybedahl <calle@init.se>
2009-06-15 14:51:24 +0200

    Add webserver table to schema file.

--- Zonestat/db/create_tables.sql
+++ Zonestat/db/create_tables.sql
@@ -5,3 +5,13 @@ CREATE TABLE IF NOT EXISTS `zone` (
         `type` varchar(10) NOT NULL,
         `data` varchar(32767) NOT NULL
     ) ENGINE=InnoDB DEFAULT CHARSET=ascii;
+
+CREATE TABLE IF NOT EXISTS `webserver` (
+    `id` serial primary key,
+    `raw` varchar(512) NOT NULL,
+    `type` varchar(255),
+    `version` varchar(255),
+    `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+    `domain_id` INT(10) unsigned,
+    CONSTRAINT `webserver_domain` FOREIGN KEY (`domain_id`) REFERENCES `domains` (`id`) ON DELETE CASCADE
+    ) ENGINE=InnoDB DEFAULT CHARSET=ascii;
