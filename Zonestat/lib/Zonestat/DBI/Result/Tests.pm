a8d074303a17be05c411059c6e255212e7382604
Calle Dybedahl <calle@init.se>
2009-06-24 11:37:37 +0200

    Parsing column wrappers for begin and end times

--- Zonestat/lib/Zonestat/DBI/Result/Tests.pm
+++ Zonestat/lib/Zonestat/DBI/Result/Tests.pm
@@ -1,13 +1,43 @@
 package Zonestat::DBI::Result::Tests;
 use base 'DBIx::Class';
+use Date::Parse;
+use POSIX qw[strftime];
 
 __PACKAGE__->load_components(qw[Core]);
 __PACKAGE__->table('tests');
 __PACKAGE__->add_columns(
-    qw[id domain begin end source_id source_data count_critical count_error
-      count_warning count_notice count_info]
+    qw[id domain source_id source_data count_critical count_error
+      count_warning count_notice count_info],
+    begin => { accessor => '_begin' },
+    end   => { accessor => '_end' },
 );
 __PACKAGE__->set_primary_key('id');
 __PACKAGE__->has_many(results => 'Zonestat::DBI::Result::Results', 'test_id');
 __PACKAGE__->belongs_to(source => 'Zonestat::DBI::Result::Source', 'source_id');
+
+sub maybe_format {
+    my ($formatted, $time) = @_;
+    my $t = str2time($time);
+
+    if ($formatted) {
+        return strftime("%y-%m-%d %H:%M:%S", localtime($t));
+    } else {
+        return $t;
+    }
+}
+
+sub begin {
+    my $self      = shift;
+    my $formatted = shift;
+
+    return maybe_format($formatted, $self->_begin);
+}
+
+sub end {
+    my $self      = shift;
+    my $formatted = shift;
+
+    return maybe_format($formatted, $self->_end);
+}
+
 1;
