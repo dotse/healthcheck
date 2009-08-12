69b5c2266d43d867171d9522ce1cc78b5741b450
Calle Dybedahl <calle@init.se>
2009-08-12 11:09:52 +0200

    Allow domain creation when adding domains to a domainset.

--- Zonestat/lib/Zonestat/DBI/Result/Domains.pm
+++ Zonestat/lib/Zonestat/DBI/Result/Domains.pm
@@ -5,6 +5,9 @@ __PACKAGE__->load_components(qw[Core]);
 __PACKAGE__->table('domains');
 __PACKAGE__->add_columns(qw[id domain last_test last_import]);
 __PACKAGE__->set_primary_key('id');
+
+__PACKAGE__->add_unique_constraint(domain => [qw(domain)]);
+
 __PACKAGE__->has_many(
     tests => 'Zonestat::DBI::Result::Tests',
     { 'foreign.domain' => 'self.domain' }
