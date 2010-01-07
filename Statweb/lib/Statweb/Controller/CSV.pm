8fa2ece46515173846e718a779a5b4ad56ea00e2
Calle Dybedahl <calle@init.se>
2010-01-07 14:57:16 +0100

    CSV links for more tables.

--- Statweb/lib/Statweb/Controller/CSV.pm
+++ Statweb/lib/Statweb/Controller/CSV.pm
@@ -23,22 +23,111 @@ Catalyst Controller.
 
 =cut
 
-sub index : Path : Args(0) {
+sub auto : Private {
     my ($self, $c) = @_;
+    my $db = $c->model('DB::Testrun');
 
-    $c->response->body('Matched Statweb::Controller::CSV in CSV.');
+    $c->stash(
+        {
+            trs => [
+                sort   { $b->tests->count <=> $a->tests->count }
+                  grep { $_ }
+                  map  { $db->find($_) }
+                  keys %{ $c->session->{testruns} }
+            ]
+        }
+    );
+
+    return 1;
 }
 
 sub webserver_software_http : Local : Args(0) {
     my ($self, $c) = @_;
+    $c->stash->{filename} = 'software_http_' . join '-',
+      keys %{ $c->session->{testruns} };
+
+    $c->forward('/testruns/webpages_software');
+    $c->stash->{csv_data} = $c->stash->{data}{software}{http};
+    $c->detach('send_csv');
+}
+
+sub webserver_software_https : Local : Args(0) {
+    my ($self, $c) = @_;
+    $c->stash->{filename} = 'software_https_' . join '-',
+      keys %{ $c->session->{testruns} };
+
+    $c->forward('/testruns/webpages_software');
+    $c->stash->{csv_data} = $c->stash->{data}{software}{https};
+    $c->detach('send_csv');
+}
+
+sub webserver_response_http : Local : Args(0) {
+    my ($self, $c) = @_;
+    $c->stash->{filename} = 'response_http_' . join '-',
+      keys %{ $c->session->{testruns} };
+
+    $c->forward('/testruns/webpages_response');
+    $c->stash->{csv_data} = $c->stash->{data}{response}{http};
+    $c->detach('send_csv');
+}
+
+sub webserver_response_https : Local : Args(0) {
+    my ($self, $c) = @_;
+    $c->stash->{filename} = 'response_https_' . join '-',
+      keys %{ $c->session->{testruns} };
+
+    $c->forward('/testruns/webpages_response');
+    $c->stash->{csv_data} = $c->stash->{data}{response}{https};
+    $c->detach('send_csv');
+}
+
+sub webserver_content_http : Local : Args(0) {
+    my ($self, $c) = @_;
+    $c->stash->{filename} = 'content_http_' . join '-',
+      keys %{ $c->session->{testruns} };
+
+    $c->forward('/testruns/webpages_content');
+    $c->stash->{csv_data} = $c->stash->{data}{content}{http};
+    $c->detach('send_csv');
+}
+
+sub webserver_content_https : Local : Args(0) {
+    my ($self, $c) = @_;
+    $c->stash->{filename} = 'content_https_' . join '-',
+      keys %{ $c->session->{testruns} };
+
+    $c->forward('/testruns/webpages_content');
+    $c->stash->{csv_data} = $c->stash->{data}{content}{https};
+    $c->detach('send_csv');
+}
+
+sub webserver_charset_http : Local : Args(0) {
+    my ($self, $c) = @_;
+    $c->stash->{filename} = 'charset_http_' . join '-',
+      keys %{ $c->session->{testruns} };
+
+    $c->forward('/testruns/webpages_charset');
+    $c->stash->{csv_data} = $c->stash->{data}{charset}{http};
+    $c->detach('send_csv');
+}
+
+sub webserver_charset_https : Local : Args(0) {
+    my ($self, $c) = @_;
+    $c->stash->{filename} = 'charset_https_' . join '-',
+      keys %{ $c->session->{testruns} };
+
+    $c->forward('/testruns/webpages_charset');
+    $c->stash->{csv_data} = $c->stash->{data}{charset}{https};
+    $c->detach('send_csv');
+}
+
+sub send_csv : Private {
+    my ($self, $c) = @_;
     my $csv      = Text::CSV_XS->new;
     my $res      = '';
-    my $filename = $c->stash->{pagetitle};
+    my $filename = $c->stash->{filename};
 
-    $filename =~ s/\W/_/;
-    $c->forward('/testruns/webpages_software');
-
-    foreach my $r (@{ $c->stash->{data}{software}{http} }) {
+    foreach my $r (@{ $c->stash->{csv_data} }) {
         $csv->combine(@$r);
         $res .= $csv->string;
         $res .= "\n";
