ac4321052143097bf4960cd6b179a9acfb77518e
Calle Dybedahl <calle@init.se>
2010-02-16 12:41:46 +0100

    Button to start a cache rebuild for all testruns in a domainset.

--- Statweb/lib/Statweb/Controller/Domainset.pm
+++ Statweb/lib/Statweb/Controller/Domainset.pm
@@ -3,6 +3,7 @@ package Statweb::Controller::Domainset;
 use strict;
 use warnings;
 use parent 'Catalyst::Controller';
+use Zonestat;
 
 =head1 NAME
 
@@ -54,7 +55,7 @@ sub delete : Chained('index') : Args(1) : PathPart('delete') {
     my ($self, $c, $domain_id) = @_;
 
     $c->stash->{dset}->remove_domain($domain_id);
-    
+
     $c->res->redirect(
         $c->uri_for_action('/domainset/first', [$c->stash->{dset}->id]));
 }
@@ -65,6 +66,33 @@ sub add : Chained('index') : Args(0) : PathPart('add') {
     my $domain = $c->model('DB::Domains')->find({ domain => $domainname });
 
     $c->stash->{dset}->add_to_glue({ domain_id => $domain->id });
+    my $trs = $c->stash->{dset}->testruns;
+    while (defined(my $tr = $trs->next)) {
+        $tr->invalidate_cache;
+    }
+
+    $c->res->redirect(
+        $c->uri_for_action('/domainset/first', [$c->stash->{dset}->id]));
+}
+
+sub rebuild : Chained('index') : Args(0) : PathPart('rebuild') {
+    my ($self, $c) = @_;
+
+    my $trs = $c->stash->{dset}->testruns;
+    my $pr  = Zonestat->new->present;
+
+    if (fork() == 0) {
+        if (fork() == 0) {
+            while (defined(my $tr = $trs->next)) {
+                $tr->invalidate_cache;
+                $pr->build_cache_for_testrun($tr);
+            }
+            exit(0);
+        } else {
+            exit(0);
+        }
+    }
+
     $c->res->redirect(
         $c->uri_for_action('/domainset/first', [$c->stash->{dset}->id]));
 }
