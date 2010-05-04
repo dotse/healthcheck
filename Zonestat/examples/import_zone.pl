8f8b07022b2d6edc85a5905e5df936f8681e528f
Calle Dybedahl <calle@init.se>
2010-05-04 15:28:14 +0200

    Script to import zone.

---
+++ Zonestat/examples/import_zone.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+use Zonestat;
+
+my $prep = Zonestat->new->prepare;
+
+if($prep->fetch_zone) {
+    $prep->db_import_zone;
+    $prep->create_random_set;
+} else {
+    print STDERR "Failed to download zone.\n";
+}
