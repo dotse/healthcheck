17ab2e0bb4e83a89c189e529f0689e7edb04e9fd
Calle Dybedahl <calle@init.se>
2010-10-22 10:01:33 +0200

    Method to put a domainset into the gathering queue.

--- Zonestat/lib/Zonestat/Domainset.pm
+++ Zonestat/lib/Zonestat/Domainset.pm
@@ -8,52 +8,59 @@ use base 'Zonestat::Common';
 use Digest::SHA1 qw[sha1_hex];
 use Try::Tiny;
 
+use Data::Dumper;
+
 sub new {
-    my $class = shift;
+    my $class  = shift;
     my $parent = shift;
-    my $self = $class->SUPER::new($parent);
+    my $self   = $class->SUPER::new($parent);
     $self->{name} = shift;
-    
+
     return $self;
 }
 
-sub name { return $_[0]->{name}}
+sub name { return $_[0]->{name} }
 
 sub db {
     my $self = shift;
-    
+
     return $self->SUPER::db('zonestat-dset');
 }
 
 sub id {
-    my $self = shift;
+    my $self   = shift;
     my $domain = shift;
-    
+
     return sha1_hex($self->name . $domain);
 }
 
 sub add {
-    my $self = shift;
+    my $self    = shift;
     my @domains = @_;
-    my $name = $self->name;
-    
-    $self->db->bulkStore([
-        map {$self->db->newDoc($self->id($_), undef, {domain => $_, set => $name})} @domains
-        ]);
-    
+    my $name    = $self->name;
+
+    $self->db->bulkStore(
+        [
+            map {
+                $self->db->newDoc($self->id($_), undef,
+                    { domain => $_, set => $name })
+              } @domains
+        ]
+    );
+
     return $self;
 }
 
 sub remove {
-    my $self = shift;
+    my $self   = shift;
     my $domain = shift;
-    
+
     my $doc = $self->db->newDoc($self->id($domain), undef);
     try {
         $doc->retrieve;
         $doc->delete;
     };
-    
+
     return $self;
 }
 
@@ -62,18 +69,36 @@ sub all {
     my $ddoc = $self->db->newDesignDoc('_design/util');
 
     $ddoc->retrieve;
-    return map {$_->{value}} @{$ddoc->queryView('set', key => $self->name)->{rows}};
+    return
+      map { $_->{value} }
+      @{ $ddoc->queryView('set', key => $self->name)->{rows} };
 }
 
 sub clear {
     my $self = shift;
-    
+
     foreach my $domain ($self->all) {
         my $doc = $self->db->newDoc($self->id($domain));
         $doc->retrieve;
         $doc->delete;
     }
-    
+
+    return $self;
+}
+
+sub enqueue {
+    my $self    = shift;
+    my $testrun = $self->run_id;
+
+    my $trdoc = $self->parent->db('zonestat-testrun')->newDoc($testrun, undef,
+        { domainset => $self->name, queued_at => time(), testrun => "" . $testrun });
+    print STDERR Dumper($trdoc);
+    $trdoc->create;
+
+    $self->parent->gather->put_in_queue(
+        map { { domain => $_, priority => 5, source_data => $testrun, } }
+          $self->all);
+
     return $self;
 }
 
