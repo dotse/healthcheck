283361431ab503a59de71470c55b722e9f489325
Calle Dybedahl <calle@init.se>
2010-10-28 10:07:34 +0200

    Utility scripts to ask CouchDB to compact or rebuild for all views in all databases.

---
+++ Zonestat/apps/compact_couchdb.pl
@@ -0,0 +1,16 @@
+#!/opt/local/bin/perl
+
+use Zonestat;
+use HTTP::Request;
+
+my $dbc = Zonestat->new->dbconn;
+
+foreach my $db (@{ $dbc->listDBs }) {
+    next unless $db->dbInfo->{db_name} =~ /^zonestat/;
+    my $url = $dbc->{uri} . $db->uriName . '/_compact';
+    print $db->uriName, ": ";
+    my $res =
+      $dbc->{ua}->request(
+        HTTP::Request->new(POST => $url, ['Content-Type', 'application/json']));
+    print $res->status_line, "\n";
+}
---
+++ Zonestat/apps/rebuild_couchdb.pl
@@ -0,0 +1,23 @@
+#!/opt/local/bin/perl
+
+use Zonestat;
+use HTTP::Request;
+
+my $dbc = Zonestat->new->dbconn;
+
+foreach my $db (@{ $dbc->listDBs }) {
+    next unless $db->dbInfo->{db_name} =~ /^zonestat/;
+    foreach my $ddoc (@{ $db->listDesignDocs(startkey => '_', endkey => '`') })
+    {
+        $ddoc->retrieve;
+        foreach my $view ($ddoc->listViews) {
+            printf(
+                "%s%s/%s/_view/%s\n",
+                $dbc->{uri},
+                $db->dbInfo->{db_name},
+                $ddoc->id, $view
+            );
+            $ddoc->queryView($view);
+        }
+    }
+}
