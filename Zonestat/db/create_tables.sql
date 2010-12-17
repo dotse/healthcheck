b902c00aaa4d0ddd2e90fd667715b61ab49ba597
Calle Dybedahl <calle@init.se>
2010-12-17 15:10:29 +0100

    Typo fix

--- Zonestat/db/create_tables.sql
+++ Zonestat/db/create_tables.sql
@@ -1,4 +1,4 @@
-/*! SET FOREIGN_KEY_CHECKS=0 */
+/*! SET FOREIGN_KEY_CHECKS=0 */;
 
 CREATE TABLE IF NOT EXISTS `zone` (
         `name` varchar(255) NOT NULL,
--- Zonestat/db/modify_tables.sql
+++ Zonestat/db/modify_tables.sql
@@ -1,4 +1,4 @@
-/*! SET FOREIGN_KEY_CHECKS=0 */
+/*! SET FOREIGN_KEY_CHECKS=0 */;
 
 ALTER TABLE domains ADD COLUMN (last_import timestamp);
 ALTER TABLE `tests` ADD COLUMN (run_id bigint(20) unsigned);
