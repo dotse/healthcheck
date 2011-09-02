bad6021636ac5882b20d543c086c9bfd81dfbe98
Calle Dybedahl <calle@init.se>
2011-09-02 14:49:11 +0200

    Make rebuild trigger script faster.

--- Zonestat/apps/rebuild_couchdb.pl
+++ Zonestat/apps/rebuild_couchdb.pl
@@ -2,16 +2,23 @@
 
 use Zonestat;
 use HTTP::Request;
+use LWP::UserAgent;
 
-my $dbc = Zonestat->new->dbconn;
+my $zs  = Zonestat->new;
+my $dbc = CouchDB::Client->new(
+    ua       => LWP::UserAgent->new( timeout => 1 ),
+    uri      => $zs->dbconfig->{url},
+    username => $zs->dbconfig->{username},
+    password => $zs->dbconfig->{password},
+);
 
 foreach my $db ( @{ $dbc->listDBs } ) {
     next unless $db->dbInfo->{db_name} =~ /^zonestat/;
     foreach my $ddoc ( @{ $db->listDesignDocs( startkey => '_', endkey => '`' ) } ) {
         $ddoc->retrieve;
-        foreach my $view ( $ddoc->listViews ) {
-            printf( "%s%s/%s/_view/%s\n", $dbc->{uri}, $db->dbInfo->{db_name}, $ddoc->id, $view );
-            eval { $ddoc->queryView( $view ); };
-        }
+        my $view = ( $ddoc->listViews )[0];
+
+        # printf( "%s%s/%s/_view/%s\n", $dbc->{uri}, $db->dbInfo->{db_name}, $ddoc->id, $view );
+        eval { $ddoc->queryView( $view ); };
     }
 }
