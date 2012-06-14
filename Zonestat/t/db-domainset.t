bd6babb92e52679d94af97831f91439cc31e23f1
Calle Dybedahl <calle@init.se>
2012-06-14 15:51:20 +0200

    Somewhat better test data.

--- Zonestat/t/db-domainset.t
+++ Zonestat/t/db-domainset.t
@@ -6,12 +6,12 @@ my $zs = new_ok('Zonestat'  => ['t/config/Config']);
 my $ds = $zs->domainset;
 isa_ok($ds, 'Zonestat::DB::Domainset');
 
-is($ds->all_sets, 0, 'No sets');
+is($ds->all_sets, 1, 'One set');
 
 my $test = $zs->domainset('test');
 $test->add('nic.se', 'iis.se', 'example.org');
 
-is_deeply($ds->all_sets, $test, 'One set');
+is_deeply([map {$_->name} $ds->all_sets], [qw(test testset)], 'One set');
 
 is_deeply($test->all, ['example.org', 'iis.se', 'nic.se'], 'Right content.');
 
@@ -19,6 +19,6 @@ $test->remove('iis.se');
 is_deeply($test->all, ['example.org', 'nic.se'], 'Can remove.');
 
 $test->clear;
-is($ds->all_sets, 0, 'No sets');
+is($ds->all_sets, 1, 'One set again');
 
 done_testing();
--- Zonestat/util/export_database.pl
+++ Zonestat/util/export_database.pl
@@ -8,11 +8,11 @@ use Try::Tiny;
 
 my $coder = JSON::XS->new->canonical->utf8->pretty;
 
-my $dbc = Zonestat->new('t/Config')->dbconn;
+my $dbc = Zonestat->new('t/config/Config')->dbconn;
 my %res;
 foreach my $db (@{$dbc->listDBs}) {
     my $i = $db->dbInfo;
-    next unless $i->{db_name} =~ /^zonestat$/;
+    next unless $i->{db_name} =~ /^zonestat/;
 
     foreach my $r (map {$_->{id}} @{$db->listDocIdRevs}) {
         my $doc = $db->newDoc($r);
@@ -20,8 +20,8 @@ foreach my $db (@{$dbc->listDBs}) {
             $doc->retrieve;
         };
         # next if $doc->id =~ /_design/;
-        push @{$res{$i->{db_name}}}, {id => $doc->id, data => $doc->data} if grep {$_==$doc->data->{testrun}} (14,24,36,475,58,78);
-        push @{$res{$i->{db_name}}}, {id => $doc->id, data => $doc->data} if $doc->id =~ /_design/;
+        push @{$res{$i->{db_name}}}, {id => $doc->id, data => $doc->data}; # if grep {$_==$doc->data->{testrun}} (14,24,36,475,58,78);
+        # push @{$res{$i->{db_name}}}, {id => $doc->id, data => $doc->data} if $doc->id =~ /_design/;
     }
 }
 
